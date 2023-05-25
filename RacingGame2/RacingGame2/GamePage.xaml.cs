a67372e4eb78ec4818dc2d82e7a4de78463616b5
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2023-05-25 12:03:03 +0200

    Repaired game screen

--- RacingGame2/RacingGame2/GamePage.xaml.cs
+++ RacingGame2/RacingGame2/GamePage.xaml.cs
@@ -1,34 +1,47 @@
-using RacingGame2.Drawables;
 using System.Diagnostics;
+using RacingGame2.Drawables;
 
 namespace RacingGame2;
 
 public partial class GamePage : ContentPage
 {
+	private float aspect = 1.5f;
+	private int screenWidth, screenHeight = 750;
+
 	public GamePage()
 	{
 		InitializeComponent();
-        
-        GraphicsView gv = new GraphicsView();
 
-		Trace.WriteLine(Application.Current.MainPage.Width);
-        Trace.WriteLine(Application.Current.MainPage.Height);
+		screenWidth = (int)(screenHeight / aspect);
+
+		GraphicsView gv = new GraphicsView();
 
         gv.VerticalOptions = LayoutOptions.Start;
         gv.HorizontalOptions = LayoutOptions.Start;
-        gv.WidthRequest = 337;
-		gv.HeightRequest = 600;
+        gv.WidthRequest = screenWidth;
+		gv.HeightRequest = screenHeight;
 		gv.Drawable = new GameDrawable();
 
+		TapGestureRecognizer gr = new TapGestureRecognizer();
+		gr.Tapped += (sender, eventArgs) =>
+		{
+			Point? relativeToContainerPosition = eventArgs.GetPosition((View)sender);
+			Trace.WriteLine(relativeToContainerPosition.Value.X);
+			Trace.WriteLine(relativeToContainerPosition.Value.Y);
+		};
+		gv.GestureRecognizers.Add(gr);
+
         Content = gv;
-    }
+	}
 
 	protected override void OnAppearing()
 	{
 		base.OnAppearing();
-		Window.Width = 337;
-		Window.Height = 600;
-		Application.Current.MainPage.WidthRequest = 337;
-		Application.Current.MainPage.HeightRequest = 600;
+		screenWidth = (int)(screenHeight / aspect);
+
+		Window.Width = screenWidth;
+		Window.Height = screenHeight;
+		Application.Current.MainPage.WidthRequest = screenWidth;
+		Application.Current.MainPage.HeightRequest = screenHeight;
 	}
 }
