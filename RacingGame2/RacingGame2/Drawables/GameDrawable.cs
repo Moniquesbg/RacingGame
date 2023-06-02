be36a9c7ceeec74ef2f741ce6de76b442fab87aa
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2023-06-02 09:35:33 +0200

    Made basic game page

--- RacingGame2/RacingGame2/Drawables/GameDrawable.cs
+++ RacingGame2/RacingGame2/Drawables/GameDrawable.cs
@@ -7,15 +7,22 @@ using System.Threading.Tasks;
 
 namespace RacingGame2.Drawables
 {
-    internal class GameDrawable : IDrawable
-    {
-        public void Draw(ICanvas canvas, RectF dirtyRect)
-        {
-            float screenWidth = (float)Application.Current.MainPage.Width;
-            float screenHeight = (float)Application.Current.MainPage.Height;
+	internal class GameDrawable : IDrawable
+	{
+		public void Draw(ICanvas canvas, RectF dirtyRect)
+		{
+			float screenWidth = (float)Application.Current.MainPage.Width;
+			float screenHeight = (float)Application.Current.MainPage.Height;
 
-            canvas.FillColor = Colors.White;
-            canvas.FillRectangle(0, 0, screenWidth, screenHeight);
-        }
-    }
+			canvas.FillColor = new Color(0.2f, 0.2f, 0.2f);
+			canvas.FillRectangle(0, 0, screenWidth, screenHeight);
+
+			canvas.StrokeColor = Colors.White;
+			canvas.StrokeSize = 5;
+			canvas.StrokeDashPattern = new float[] { 10, 10 };
+			canvas.DrawLine(screenWidth / 4, 0, screenWidth / 4, screenHeight);
+			canvas.DrawLine((screenWidth / 4) * 2, 0, (screenWidth / 4) * 2, screenHeight);
+			canvas.DrawLine((screenWidth / 4) * 3, 0, (screenWidth / 4) * 3, screenHeight);
+		}
+	}
 }
--- RacingGame2/RacingGame2/GamePage.xaml.cs
+++ RacingGame2/RacingGame2/GamePage.xaml.cs
@@ -16,22 +16,15 @@ public partial class GamePage : ContentPage
 
 		GraphicsView gv = new GraphicsView();
 
-        gv.VerticalOptions = LayoutOptions.Start;
-        gv.HorizontalOptions = LayoutOptions.Start;
-        gv.WidthRequest = screenWidth;
+		gv.VerticalOptions = LayoutOptions.Start;
+		gv.HorizontalOptions = LayoutOptions.Start;
+		gv.WidthRequest = screenWidth;
 		gv.HeightRequest = screenHeight;
 		gv.Drawable = new GameDrawable();
 
-		TapGestureRecognizer gr = new TapGestureRecognizer();
-		gr.Tapped += (sender, eventArgs) =>
-		{
-			Point? relativeToContainerPosition = eventArgs.GetPosition((View)sender);
-			Trace.WriteLine(relativeToContainerPosition.Value.X);
-			Trace.WriteLine(relativeToContainerPosition.Value.Y);
-		};
-		gv.GestureRecognizers.Add(gr);
+		getMouseCoord(gv);
 
-        Content = gv;
+		Content = gv;
 	}
 
 	protected override void OnAppearing()
@@ -39,9 +32,38 @@ public partial class GamePage : ContentPage
 		base.OnAppearing();
 		screenWidth = (int)(screenHeight / aspect);
 
-		Window.Width = screenWidth;
-		Window.Height = screenHeight;
+		Window.Width = screenWidth + 75;
+		Window.Height = screenHeight + 75;
 		Application.Current.MainPage.WidthRequest = screenWidth;
 		Application.Current.MainPage.HeightRequest = screenHeight;
+	}
+
+	void getMouseCoord(View view)
+	{
+		DragGestureRecognizer dragGestureRecognizer = new DragGestureRecognizer();
+		PointerGestureRecognizer pointerGestureRecognizer = new PointerGestureRecognizer();
+		Point startPoint, endPoint;
+
+		pointerGestureRecognizer.PointerEntered
+			+= (s, e) =>
+			{
+				startPoint = new Point(e.GetPosition((View)s).Value.X, e.GetPosition((View)s).Value.Y);
+				Trace.WriteLine(startPoint);
+			};
+
+		//dragGestureRecognizer.DragStarting += (sender, eventArgs) =>
+		//{
+
+		//	//Point? relativeToContainerPosition = eventArgs.GetPosition((View)sender);
+		//	eventArgs.Data.Text = "dsadas";
+		//	//point = new Point(relativeToContainerPosition.Value.X, relativeToContainerPosition.Value.Y);
+		//	//Trace.WriteLine(point.ToString());
+		//};
+
+		view.GestureRecognizers.Add(pointerGestureRecognizer);
+	}
+
+	void move(Point point)
+	{
 	}
 }
