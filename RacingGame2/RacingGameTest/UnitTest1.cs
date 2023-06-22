beb84c22db3e4c18532cbfcec0ae970eb13a0082
Chris <chriscrossje2001@gmail.com>
2023-06-22 21:48:19 +0200

    Added working unit test project with dummy test

---
+++ RacingGame2/RacingGameTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace RacingGameTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            bool check = true;
+            Assert.IsTrue(check);
+        }
+    }
+}
