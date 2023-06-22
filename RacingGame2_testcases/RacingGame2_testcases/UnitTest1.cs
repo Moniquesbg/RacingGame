110b6b54879d475f100ece7a18cbd71632f50fc6
Monique <89770769+Moniquesbg@users.noreply.github.com>
2023-06-22 20:13:03 +0200

    Added NUnit test environment

---
+++ RacingGame2_testcases/RacingGame2_testcases/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace RacingGame2_testcases;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
