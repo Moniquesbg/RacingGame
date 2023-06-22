35dc21fcbbe91e5f5775ab752caf37ab6faa6e0c
Chris <chriscrossje2001@gmail.com>
2023-06-22 12:48:36 +0200

    Added about page

--- RacingGame2/RacingGame2/App.xaml.cs
+++ RacingGame2/RacingGame2/App.xaml.cs
@@ -6,7 +6,7 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new HighscorePage();
+		MainPage = new MainPage();
 	}
 
     protected override Window CreateWindow(IActivationState activationState)
